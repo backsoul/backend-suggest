1c407c547c1c4bef3409281691d005f4f57ef48c
Daniel Sarmiento <danielsarmientoenterprise@gmail.com>
2024-01-04 11:17:01 -0500

    commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://suggest-p5z0jtwd2-backsoul.vercel.app"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Cargar datos de productos desde un archivo JSON
 with open('data.json', 'r') as file:
@@ -46,8 +46,9 @@ def get_similar_products(input_producto, num_similar=5, similarity_threshold=0.2
 def get_all_productos():
     return jsonify(productos)
 
+
 @app.route('/', methods=['GET'])
-def get_all_productos():
+def get_index():
     return "Hello World!"
 
 
